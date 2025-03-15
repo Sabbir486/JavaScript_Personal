2dddc25339c69a97cf7157df89637005274e0eea
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2025-03-16 04:12:12 +0600

    Add files via upload

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accId = "46486"
+let accEmail = "sabbir@gmail.com"
+var accPass = "12345"  //Prefer not to use it.
+
+let accState
+
+accCity = "Dhaka"
+
+// accId = 2  // const can not be changed.
+accEmail = "ra@gmail.com"
+accPass = "65432"
+accCity = "Rajshahi"
+
+console.log(accId);
+
+console.table([accEmail, accPass, accCity, accState])
+
+
